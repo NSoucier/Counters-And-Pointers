acf8bb78ce5f2bd5e0231903f7d2643341c89042
Nathan Soucier <soucienp@gmail.com>
2024-07-05 16:11:43 -0400

    finished problem 2

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,18 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(array, target) {
+    let left = 0;
+    let right = array.length - 1;
+
+    while (left < right) {
+        let avg = ( array[left] + array[right] ) / 2;
+        if (avg === target) {
+            return true
+        } else if (avg > target) {
+            right --;
+        } else {
+            left ++;
+        }
+    }
+
+    return false
 }
