a79ba339c007852722bf3ef6ce687d2895bca011
Nathan Soucier <soucienp@gmail.com>
2024-07-05 16:05:16 -0400

    finished first problem

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,19 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(msg, letters) {
+    const msgFreq = freq(msg);
+    const lettersFreq = freq(letters);
+
+    for (let char in msgFreq) {
+        if ( !(msgFreq[char] <= lettersFreq[char]) ) return false
+    }
+
+    return true    
+}
+
+function freq(str) {
+    const obj = {};
+    for (let char of str) {
+        obj[char] = (obj[char] +1) || 1
+    }
+    return obj
 }
