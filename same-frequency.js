e5fd787d12f17f08b4195d3b31c2b786e0ea7046
Nathan Soucier <soucienp@gmail.com>
2024-07-05 16:24:28 -0400

    finished problem #4

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,22 @@
 // add whatever parameters you deem necessary
-function sameFrequency() {
+function sameFrequency(int1, int2) {
+    const int1Freq = freq(int1);
+    const int2Freq = freq(int2);
+
+    for (let num in int1Freq) {
+        if ( int1Freq[num] !== int2Freq[num]) return false
+    }
+
+    return true
+}
+
+function freq(int) {
+    const obj = {};
+    let str = int.toString();
+
+    for (let num of str) {
+        obj[num] = ( obj[num] +1) || 1;
+    }
+
+    return obj
 }
