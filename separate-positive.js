fb5b6818ad65057d871d086e76db18c3615e2b42
Nathan Soucier <soucienp@gmail.com>
2024-07-05 16:52:43 -0400

    finished problem #5

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,23 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(array) {
+    let left = 0;
+    let right = array.length -1;
+    let current = array[left];
+    let temp
+
+    while (left < right) {
+        if (current > 0) {
+            left ++;
+            current = array[left];
+        } else {
+            temp = array[right];
+            array[right] = current;
+            right --;
+            array[left] = temp;
+            current = array[left];
+
+        }
+    }
+
+    return array
 }
