bea18f1f517a16644f6f4171d78455e996eaad3f
Nathan Soucier <soucienp@gmail.com>
2024-07-08 09:17:32 -0400

    completed question 6

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,20 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(str1, str2) {
+  let index1 = 0;
+  let index2 = 0;
+
+  while (index2 < str2.length) {
+    if (str1[index1] === str2[index2]) {
+      index1++;
+      index2++;
+    } else {
+      index2++;
+    }
+
+    if (index1 === str1.length) {
+      return true;
+    }
+  }
+
+  return false;
 }
